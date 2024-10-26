11eb56671a395d8ae772df30740d95065488e79d
iigmir <roc120j@gmail.com>
2024-10-26 23:29:17 +0800

    about i18n

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="router-about">
         <h1>{{ $t("about.title") }}</h1>
-        <p>我想做一些可以在東方夜雀食堂速查的資訊，所以做了這專案。就這樣。</p>
-        <p>程式放在 <a href="https://github.com/iigmir/mystia-hayami-app">Github</a> 上，並以 <a href="https://www.mozilla.org/en-US/MPL/2.0/">MPL-2.0</a> 授權。</p>
+        <p v-html="$t('about.description1')"></p>
+        <p v-html="$t('about.description2')"></p>
     </div>
 </template>
